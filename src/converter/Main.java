58e5a81c19e78beb0bf7a827ad7e35031f7c5045
Gao-Yuying <gyy4real@hotmail.com>
2020-06-27 18:44:09 +0800

    Refactor: file name is entered by user.

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -7,15 +7,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Converter converter = new Converter();
-        for (String file : args) {
-            converter.convert(getInput(file));
-            System.out.println("Converting result of " + file + ":");
+        Scanner scanner = new Scanner(System.in);
+        String cmd;
+        while (true) {
+            System.out.println("Enter file name (or 'exit' to quit):");
+            cmd = scanner.nextLine().trim();
+            if ("exit".equals(cmd)) { break; }
+            converter.convert(getInput(cmd));
+            System.out.println("Converting result of " + cmd + ":");
             System.out.println("\n(In document form)\n");
             System.out.println(converter.getDocument());
             System.out.println("\n(In element hierarchy form)\n");
             System.out.println(converter.getNodeInfo());
         }
-
     }
 
     static String getInput(String file) {
